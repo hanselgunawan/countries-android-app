81a6dcae5775b16f1ede0867d3d6136f966d0004
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2020-07-19 16:59:22 -0700

    removed unused imports

--- app/src/main/java/com/hanseltritama/countriesapp/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/hanseltritama/countriesapp/viewmodel/ListViewModel.kt
@@ -5,13 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.hanseltritama.countriesapp.model.CountriesRepository
-import com.hanseltritama.countriesapp.model.CountriesService
 import com.hanseltritama.countriesapp.model.Country
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-import io.reactivex.observers.DisposableSingleObserver
-import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.Dispatchers
 
 class ListViewModel : ViewModel() {
